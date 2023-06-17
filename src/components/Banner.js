fe450c55b2a12e695df39ec408c2b96615251a54
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2023-06-17 14:19:50 +0200

    Composant Banner: mise du style aux normes du BEM + passage du CSS au SCSS.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,20 +4,20 @@ import logo from '../assets/logo.png'
 
 function Banner() {    
     function UnderlineHomeLink() {
-        const underlinedHomeLink = document.querySelector('.kasa-link-home')
-        const underlinedAboutUsLink = document.querySelector('.kasa-link-aboutus')
+        const underlinedHomeLink = document.querySelector('.banner__navlink--home')
+        const underlinedAboutUsLink = document.querySelector('.banner__navlink--aboutus')
         underlinedHomeLink.style.textDecoration = 'underline'
         underlinedAboutUsLink.style.textDecoration = 'none'
     }
     function UnderlineAboutUsLink() {
-        const underlinedHomeLink = document.querySelector('.kasa-link-home')
-        const underlinedAboutUsLink = document.querySelector('.kasa-link-aboutus')
+        const underlinedHomeLink = document.querySelector('.banner__navlink--home')
+        const underlinedAboutUsLink = document.querySelector('.banner__navlink--aboutus')
         underlinedAboutUsLink.style.textDecoration = 'underline'
         underlinedHomeLink.style.textDecoration = 'none'
     }
     function LoadPagesLinks() {
-        const underlinedHomeLink = document.querySelector('.kasa-link-home')
-        const underlinedAboutUsLink = document.querySelector('.kasa-link-aboutus')
+        const underlinedHomeLink = document.querySelector('.banner__navlink--home')
+        const underlinedAboutUsLink = document.querySelector('.banner__navlink--aboutus')
         if (window.location.pathname !== '/' && window.location.pathname !== '/aboutus') {
             underlinedHomeLink.style.textDecoration = 'none'
             underlinedAboutUsLink.style.textDecoration = 'none'
@@ -33,14 +33,14 @@ function Banner() {
     }
 
     return (
-        <div className='banner-wrapper' onLoad={LoadPagesLinks}>
-        <div className='kasa-banner'>
-            <img src={logo} alt='Logo de Kasa' className='kasa-logo' />
-            <nav className='kasa-nav'>
-                <Link to="/" className='kasa-link kasa-link-home' onClick={UnderlineHomeLink}>Accueil</Link>
-                <Link to="/aboutus" className='kasa-link kasa-link-aboutus' onClick={UnderlineAboutUsLink}>A Propos</Link>
-            </nav>
-        </div>
+        <div className='banner__wrapper' onLoad={LoadPagesLinks}>
+            <div className='banner'>
+                <img src={logo} alt='Logo de Kasa' className='banner__logo' />
+                <nav className='banner__navigation'>
+                    <Link to="/" className='banner__navlinks banner__navlink--home' onClick={UnderlineHomeLink}>Accueil</Link>
+                    <Link to="/aboutus" className='banner__navlinks banner__navlink--aboutus' onClick={UnderlineAboutUsLink}>A Propos</Link>
+                </nav>
+            </div>
         </div>
     )
 }
