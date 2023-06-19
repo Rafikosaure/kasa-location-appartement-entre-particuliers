8322b5695bf851ad2b69e0c373e25d78a0071f63
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2023-06-19 11:33:35 +0200

    Styles du Footer: application de la convention BEM et passage du code du CSS vers le SCSS.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ import LogoFooter from '../assets/logo-footer.png'
 
 function Footer() {
     return (
-      <div className="div-footer">
-        <img src={LogoFooter} alt='Logo de Kasa' className='logo-footer' />
-        <p className='text-footer'>© 2020 Kasa. All rights reserved</p>
+      <div className="footer">
+        <img src={LogoFooter} alt='Logo de Kasa' className='footer__logo' />
+        <p className='footer__text'>© 2020 Kasa. All rights reserved</p>
       </div>
     );
   }
