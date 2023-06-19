ca0d0e7650285275f5123e9f9372622ff04e3091
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2023-06-19 16:15:12 +0200

    Styles de la page Error: application de la convention BEM et passage du code du CSS vers le SCSS.

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -4,13 +4,12 @@ import { Link } from 'react-router-dom'
 
 function Error() {
     return (
-        <div className="error__page">
+        <div className="error">
             <div className='error__body'>
                 <img className='error__image' src={Error404} alt='Erreur 404' />
                 <h1 className='error__title'>Oups! La page que vous demandez n'existe pas.</h1>
                 <Link className='error__redirectlink' to='/' >Retourner sur la page d'accueil</Link>
             </div>
-            
         </div>
     )
 }
