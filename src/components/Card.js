d5236f29c501e989553f123bb2aca1810bd7dd99
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2023-06-21 10:04:45 +0200

    Retrait de la fonction de redirection dans "Card" et son remplacement par une balise <Link></Link> avec rectification du SCSS associé.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,16 @@
 import '../styles/Card.scss'
 import Overlay from '../assets/RectangleCard.png'
+import { Link } from 'react-router-dom';
+
 
 function Card({ apartment }) {
-  function toApartmentPage() {
-    document.location.href=`/apartment/${apartment.id}`
-    console.log(apartment.id, apartment.title)
-  }
   return (
-    <div className='card' onClick={toApartmentPage}>
-      <img className='card__overlay' src={ Overlay } alt="Overlay" />
-      <img className='card__image' src={ apartment.cover } alt="Intérieur d'un appartement" />
-      <p className='card__title'>{ apartment.title }</p>
+    <div className='card'>
+      <Link to={`/apartment/${apartment.id}`} className='card card__link'>
+        <img className='card__overlay' src={ Overlay } alt="Overlay" />
+        <img className='card__image' src={ apartment.cover } alt="Intérieur d'un appartement" />
+        <p className='card__title'>{ apartment.title }</p>
+      </Link>
     </div>
   );
 }
