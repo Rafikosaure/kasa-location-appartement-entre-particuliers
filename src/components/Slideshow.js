723fa5fc27c5742825c37425a0a4348cc63823a6
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2023-06-21 19:46:07 +0200

    Création du composant du carroussel ("Slideshow"), et implémentation de son fonctionnement : affichage des images spécifiques à l'appartement sélectionné, boutons de navigation (précédent / suivant) permettant de parcourir la galerie indéfiniment dans les deux sens, boutons qui sont absents lorsqu'il n'y a qu'une seule image disponible. Egalement : début de la mise en place des styles du carroussel.

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -1,12 +1,42 @@
 import '../styles/Slideshow.scss'
+import { useState } from 'react';
 
 function Slideshow({ currentDatas }) {
+    const images = currentDatas.pictures
+    let [currentImage, letCurrentImage] = useState(0)
+    let [oneOnly, setOneOnly] = useState(true)
+
+    function PrevImage() {
+        if (currentImage <= 0) {
+            letCurrentImage(currentImage = images.length -1)
+        }
+    }
+    function NextImage() {
+        if (currentImage >= images.length -1) {
+            letCurrentImage(currentImage = 0)
+        }
+    }
+    function SwitchContent() {
+        if (images.length === 1) {
+            setOneOnly(oneOnly = false)
+        }
+    }
+
     return (
         <div className="slideshow">
-            <div>{currentDatas.title}</div>
+            {oneOnly ? (
+                <div className='slideshow__switchcontent' onLoad={SwitchContent}>
+                    <button className='slideshow__button slideshow__button--prev' onClick={() => {letCurrentImage(currentImage - 1); PrevImage()}}>Image précédante</button>
+                    <img className='slideshow__currentImage' src={images[currentImage]} alt='Photos du logement courant du caroussel' />
+                    <button className='slideshow__button slideshow__button--next' onClick={() => {letCurrentImage(currentImage + 1); NextImage()}}>Image suivante</button>
+                </div>
+            ) : (
+                <div className='slideshow__switchcontent' onLoad={SwitchContent}>
+                    <img className='slideshow__currentImage' src={images[currentImage]} alt='Photos du logement courant du caroussel' />
+                </div>
+            )}
         </div>
-      
-    );
-  }
+    )
+}
   
-  export default Slideshow;
+export default Slideshow
