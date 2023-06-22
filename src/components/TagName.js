9ae4ac53aee6588bdd7da2146a1026f42f498ade
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2023-06-22 22:19:48 +0200

    Mise en place de la structure de base de la page "Apartment" et des champs où apparaitront les datas.

---
+++ src/components/TagName.js
@@ -0,0 +1,12 @@
+// import Datas from '../datas/logements.json'
+import '../styles/TagName.scss'
+
+function TagName() {
+    return (
+        <div className='tagname'>
+            <p className='tagname__text'>Chocolatine</p>
+        </div>
+    )
+}
+  
+export default TagName
--- src/pages/Apartment.js
+++ src/pages/Apartment.js
@@ -1,13 +1,39 @@
 import { useParams } from "react-router-dom"
 import '../styles/Apartment.scss'
 import Slideshow from "../components/Slideshow"
-import datas from '../datas/logements.json'
+import Datas from '../datas/logements.json'
+import TagName from "../components/TagName"
 
 function Apartment() {
   const { apartmentId } = useParams()
   return (
     <div className='apartment'> 
-      <Slideshow currentDatas = {datas.find(apartment => apartment.id === apartmentId)} />
+      <Slideshow currentDatas = {Datas.find(apartment => apartment.id === apartmentId)} />
+      <div className="apartment__datas">
+        <div className="apartment__datas--orientation">
+          <div>
+            <h1 className="apartment__title">Couroucoucou Steuch Steuch</h1>
+            <p className="apartment__description">Apart de misère, passez votre chemin !</p>
+            <div className="apartment__Tagnames">
+              <TagName />
+              <TagName />
+              <TagName />
+            </div>
+          </div>
+          <div>
+            <div className="apartment__host">
+              <p className="apartment__hostName">Nom de l'hôte</p>
+              <img className="apartment__hostPhoto" src="" alt="" />
+            </div>
+            <span>Stars</span>
+          </div>
+        </div>
+        <div className="apartment__collapseComponents">
+          <p className="apartement__collapse">C'est une bonne situation ça, scribe ?</p>
+          <p className="apartement__collapse">Je ne crois pas qu'il y ai de bonnes ou de mauvaises situations.</p>
+        </div>
+        
+      </div>
     </div>
   )
 }
