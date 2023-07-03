e7342d1962cd17d98ef2e064e1880f09b0ce2233
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2023-07-03 20:23:24 +0200

    Intégration des medias queries pour le composant Landscape sur les pages Home et About Us.

--- src/components/Landscape.jsx
+++ src/components/Landscape.jsx
@@ -1,21 +1,33 @@
 import '../styles/Landscape.scss'
 import LandscapeHome from '../assets/landscape-home.jpg'
 import LandscapeAboutUs from '../assets/landscape-aboutus.jpg'
+import { useState } from 'react'
+// import { useRef } from 'react'
+import { useEffect } from 'react'
 
 function Landscape() {
-    let landscapeImage = LandscapeHome
-    let imageLegend = 'Chez vous, partout et ailleurs'
-    if (window.location.pathname === '/aboutus') {
-        landscapeImage = LandscapeAboutUs
-        imageLegend = ''
-    }
+    const aboutusPage = window.location.pathname === '/aboutus'
+    const [landscapeImage, setLandscapeImage] = useState(LandscapeHome)
+    const [homeStyle, updateHomeStyle] = useState('')
+    const [imageLegend, setImageLegend] = useState('Chez vous, partout et ailleurs')
+
+    // Gestion des styles du composant selon la page & les media queries
+    useEffect(() => {
+        if (aboutusPage) {
+            setLandscapeImage(LandscapeAboutUs)
+            setImageLegend('')
+        } else {
+            updateHomeStyle('landscape--homestyle')
+        }
+    }, [aboutusPage])
+    
     return (
-        <div className="landscape">
+        <div className={`landscape ${homeStyle}`}>
             <div className="landscape__overlay">
                 <img
                     className="landscape__image"
                     src={landscapeImage}
-                    alt="Littoral rocheux secoué par les vagues"
+                    alt="Paysage de présentation"
                 />
             </div>
             <p className="landscape__legend">{imageLegend}</p>
